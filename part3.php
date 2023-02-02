0d877456624d3137dc4d6d99e1958aba54f47db3
matsubarajun <123618367+matsubarajun@users.noreply.github.com>
2023-02-02 13:50:25 +0900

    Update part3.php

--- part3.php
+++ part3.php
@@ -4,7 +4,7 @@ $arr という配列の仮引数を持ち、数値が入った配列array(1, 3, 
 その要素をすべてかけた結果を返す関数を作成してください
 */
 function count_array($arr){
-    $result = $arr[0];
+    $result = 1;
 
     foreach($arr as $arr){
         $result *= $arr;
