4d15b61ea1583ccea9832cfef38526bcb64f2f44
matsubarajun <123618367+matsubarajun@users.noreply.github.com>
2023-02-02 14:05:35 +0900

    Update part1.php

--- part1.php
+++ part1.php
@@ -2,9 +2,9 @@
 /*
 引数に数値を指定して実行すると、数値を2倍にして返す関数を作成してください
 */
-function sum($max){
+function int($max){
     $result = 0;
     $result = $max*2;
     return $result;
 }
-echo sum(30);
+echo int(30);
